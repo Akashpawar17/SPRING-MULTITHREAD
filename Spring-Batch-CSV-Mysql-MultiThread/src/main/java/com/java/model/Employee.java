3761443423b187063d76935e3cbc34e9865ed381
akpowar <akpowar@DIN66009897.corp.capgemini.com>
2021-01-29 10:23:34 +0530

    spring batch

---
+++ Spring-Batch-CSV-Mysql-MultiThread/src/main/java/com/java/model/Employee.java
@@ -0,0 +1,62 @@
+package com.java.model;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	Integer id;
+	String firstName;
+	String lastName;
+	String department;
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", department=" + department + "]";
+	}
+
+}
